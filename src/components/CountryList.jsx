aff4fedb4f3223de4cd75fdd0c414bfe9da67e91
TharunNookala <iamtharun7799@gmail.com>
2023-06-23 16:55:49 +0530

    context API-useReducer implemented

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,37 @@
+import CountryItem from './CountryItem'
+import styles from './CountryList.module.css'
+import Spinner from './Spinner'
+import Message from './Message'
+import { useCities } from '../contexts/CitiesContext'
+
+const CountryList = () => {
+    const { cities, isLoading } = useCities();
+    if (isLoading) return <Spinner />
+
+    if (!cities.length)
+        return (
+            <Message message="Add your First city by clicking on a city on a map" />
+        );
+
+
+    const countries = cities.reduce((arr, city) => {
+        if (!arr.map((el) => el.country).includes(city.country))
+            return [...arr, { country: city.country, emoji: city.emoji }];
+        else return arr;
+    }, []);
+
+    return (
+        <>
+            <div>Hello</div>
+            <ul className={styles.countryList}>
+                {
+                    countries.map((country) => (
+                        <CountryItem country={country} key={country.country} />
+                    ))
+                }
+            </ul>
+        </>
+    )
+}
+
+export default CountryList
